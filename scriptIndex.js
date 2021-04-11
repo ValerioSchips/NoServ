ad3d89171b98ec3bc5cb36a8d63bd0addfbbea4c
ValerioSchips <valerio_schips@hotmail.it>
2021-04-11 19:34:20 +0200

    test

--- scriptIndex.js
+++ scriptIndex.js
@@ -3,11 +3,13 @@ var cookieBanner;
 var storage = window.sessionStorage;
 window.onload = (event) => {
   closeCookieBanner = document.getElementById("AcceptBanner");
-  cookieBanner = document.getElementById("bannerContainer");
+  cookieBanner = document.getElementById("banner");
 
   if (storage.getItem("cookie") != "sown") {
-    cookieBanner.innerHTML = '<div id="banner" style="display: none"> <p> This sites uses Google Analytics to collect cookies. <br> Collected data will be used to analyse users action on the sites. <br> Collected data are IP, <a href=’insert-link’>cookie policy</a> </p> <button id="AcceptBanner" onclick="closeBanner()">Accept</button>';
+    cookieBanner.style.display = "block";
+    //cookieBanner.innerHTML = '<div id="banner" style="display: none"> <p> This sites uses Google Analytics to collect cookies. <br> Collected data will be used to analyse users action on the sites. <br> Collected data are IP, <a href=’insert-link’>cookie policy</a> </p> <button id="AcceptBanner" onclick="closeBanner()">Accept</button>';
   }else {
+    cookieBanner.style.display = "none";
     cookiesAccepted();
   };
 };
