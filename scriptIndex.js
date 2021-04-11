181b97b6792e44f18557ac3c99bf544531da3be7
ValerioSchips <valerio_schips@hotmail.it>
2021-04-11 19:28:52 +0200

    test

--- scriptIndex.js
+++ scriptIndex.js
@@ -3,21 +3,22 @@ var cookieBanner;
 var storage = window.sessionStorage;
 window.onload = (event) => {
   closeCookieBanner = document.getElementById("AcceptBanner");
-};
-
-$(document).ready(function() {
-  cookieBanner = document.getElementById("banner");
+  cookieBanner = document.getElementById("bannerContainer");
 
   if (storage.getItem("cookie") != "sown") {
-    cookieBanner.style.display="block";
+    cookieBanner.innerHTML = '<div id="banner" style="display: none"> <p> This sites uses Google Analytics to collect cookies. <br> Collected data will be used to analyse users action on the sites. <br> Collected data are IP, <a href=’insert-link’>cookie policy</a> </p> <button id="AcceptBanner" onclick="closeBanner()">Accept</button>';
   }else {
     cookiesAccepted();
   };
-});
+};
+
+//$(document).ready(function() {
+
+//});
 
 function closeBanner(){
   storage.setItem("cookie","shown");
-  cookieBanner.style.display="none";
+  cookieBanner.innerHTML = '';
   cookiesAccepted();
 }
 
